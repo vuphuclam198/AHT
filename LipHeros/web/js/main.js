170a408647eb3c421b54516c4322fb2ddfa62d28
vuphuclam198 <vuphuclam98@gmail.com>
2021-07-26 07:59:08 +0700

    theme update

--- LipHeros/web/js/main.js
+++ LipHeros/web/js/main.js
@@ -3,51 +3,27 @@ require([
     'slick',
     'domReady!'
 ], function ($) {
-
-    $(".featured-items").slick({
-        dots: false,
+    $(".widget-product-grid").slick({
+        dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-                         
-                       
-                       
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                slidesToShow: 1,
-                            }
-                        }
-
-                        
-                    ]
-    });
-    $(".widget-product-grid").slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        responsive: [
-                         
-                        {
-                            breakpoint: 1440,
-                            settings: {
-                                slidesToShow: 4,
-                            }
-                        },
                         {
                             breakpoint: 1024,
                             settings: {
                                 slidesToShow: 3,
+                                slidesToScroll: 1,
+                                dots: true,
+                                infinite: true
                             }
                         },
                         {
                             breakpoint: 768,
                             settings: {
-                                slidesToShow: 1,
+                                slidesToShow: 2,
+                                slidesToScroll: 1
                             }
                         }
 
