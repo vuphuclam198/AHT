5457eca7502561638a1563be3b6bf96ea7794185
vuphuclam198 <vuphuclam98@gmail.com>
2021-07-28 16:29:40 +0700

    responsive all

--- LipHeros/web/js/main.js
+++ LipHeros/web/js/main.js
@@ -51,43 +51,34 @@ require([
                     ]
     });
 
-        // slick product instagram
-        $(".instagram-content").slick({
-            dots: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            responsive: [
-                            {
-                                breakpoint: 1024,
-                                settings: {
-                                    slidesToShow: 4,
-                                    slidesToScroll: 1,
-                                    dots: false,
-                                    infinite: true
-                                }
-                            },
-                            {
-                                breakpoint: 768,
-                                settings: {
-                                    slidesToShow: 3,
-                                    slidesToScroll: 1,
-                                    dots: false,
-                                    infinite: true
-                                }
-                            },
-                            {
-                                breakpoint: 480,
-                                settings: {
-                                    slidesToShow: 2,
-                                    slidesToScroll: 1,
-                                    dots: false,
-                                    infinite: true
-                                }
+    // slick product instagram
+    $(".instagram-content").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 4,
+                                slidesToScroll: 1,
+                                dots: false,
+                                infinite: true
                             }
-                        ]
-        });
+                        },
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1,
+                                dots: false,
+                                infinite: true
+                            }
+                        }
+                    ]
+    });
 
     $(".services-slide").slick({
         dots: false,
