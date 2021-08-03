11f828be245240876c5cf306300e98ef88a3f8ee
vuphuclam198 <vuphuclam98@gmail.com>
2021-08-03 17:59:26 +0700

    list category

--- LipHeros/web/js/main.js
+++ LipHeros/web/js/main.js
@@ -80,6 +80,7 @@ require([
                     ]
     });
 
+    // slick service slide main top
     $(".services-slide").slick({
         dots: false,
         infinite: false,
@@ -88,9 +89,89 @@ require([
         slidesToScroll: 1,
     });
 
+    // display search heeader
     $(".search").click(function() {
         $("#search").addClass("show");
         $(".search").addClass("active");
     });
 
+    // sort listing
+    $('.sorter-label').click(function(){
+        $('.toolbar-sorter').toggleClass('show');
+    });
+
+    // more seo text
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("more-btn");
+    
+    $('#more-btn').click(function () {
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "Read more"; 
+            moreText.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "Read less"; 
+            moreText.style.display = "inline";
+        }
+    });
+
+    // // product-item -slick
+    // $(".product-items").slick({
+    //     dots: false,
+    //     infinite: true,
+    //     speed: 300,
+    //     slidesToShow: ,
+    //     slidesToScroll: 1,
+    //     responsive: [
+    //                     {
+    //                         breakpoint: 1024,
+    //                         settings: {
+    //                             slidesToShow: 2,
+    //                             slidesToScroll: 1,
+    //                             dots: false,
+    //                             infinite: true
+    //                         }
+    //                     },
+    //                     {
+    //                         breakpoint: 768,
+    //                         settings: {
+    //                             slidesToShow: 3,
+    //                             slidesToScroll: 1,
+    //                             dots: false,
+    //                             infinite: true
+    //                         }
+    //                     }
+    //                 ]
+    // });
+
+    // question accodion
+    $('#question-title1').click(function () {
+        $('#question-content__item1').toggleClass('active');
+    });
+
+    $('#question-title2').click(function () {
+        $('#question-content__item2').toggleClass('active');
+    });
+
+    $('#question-title3').click(function () {
+        $('#question-content__item3').toggleClass('active');
+    });
+
+    $('#question-title4').click(function () {
+        $('#question-content__item4').toggleClass('active');
+    });
+
+    $('#question-title5').click(function () {
+        $('#question-content__item5').toggleClass('active');
+    });
+
+    $('#question-title6').click(function () {
+        $('#question-content__item6').toggleClass('active');
+    });
+
+    $('.author-account').click(function() {
+        $('.author-account').toggleClass('active');
+    });
 });
